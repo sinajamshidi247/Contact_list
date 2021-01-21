dc7ea3d1019acff43df9065aa475d6002322e2f0
sina jamshdi <mentalic.pro@gmail.com>
2021-01-21 12:25:37 +0330

    edit,delete features

--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('',views.index,name = "index"),
     path('add/',views.add,name = "add"),
     path('contact-profile/<int:pk>/',views.cotact_profile,name = "profile"),
+    path('<int:pk>/edit/',views.edit,name = "edit"),
+    path('<int:pk>/delete/',views.delete,name = "delete")
+
+
 
 
 ]
--- contact/views.py
+++ contact/views.py
@@ -8,8 +8,15 @@ def index(request):
 
     contacts = Contact.objects.all()
 
+    search_input = request.GET.get('search_area')
+    if search_input :
+        contacts = Contact.objects.filter(full_name_icontains=search_input)
+    else:
+        contacts = Contact.objects.all()
+        search_input = ''
 
-    return render(request, 'index.html',{'contacts':contacts})
+
+    return render(request, 'index.html',{'contacts':contacts,'search_input':search_input})
 
 
 
@@ -36,3 +43,28 @@ def cotact_profile(request,pk):
 
 
     return render(request,'contact-profile.html',{'contacts':contacts})
+
+def edit(request,pk):
+    contacts = Contact.objects.get(id=pk)
+    if request.method == "POST":
+        
+        contacts.full_name = request.POST['fullname']
+        contacts.realation = request.POST['relationship']
+        contacts.adress = request.POST['address']
+        contacts.email = request.POST.get('e-email', False)
+        contacts.number = request.POST['phone-number']
+        contacts.save()
+        return redirect('contact:index')
+
+
+    return render(request,'edit.html',{'contacts':contacts})
+
+
+
+def delete(request,pk):
+    contacts = Contact.objects.get(id=pk)
+
+    if request.method == "POST":
+        contacts.delete()
+        return redirect('contact:index')
+    return render(request,'delete.html',{'contacts':contacts})
